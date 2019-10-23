e019e435c0498d19c83ab0478259b07ac4bac51f
Marius Radu <marius.radu@teleporthq.io>
2019-10-23 13:53:49 +0300

    partial artefacts for CI CD

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\work_2019\tlphq\01_hr_bt\pythonapps\goodbye_django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
